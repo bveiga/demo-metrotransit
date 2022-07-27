30109654128f5e6844822ce0b86740bf3515b97a
Bruno Veiga <bspveiga@gmail.com>
2022-07-27 16:17:11 -0500

    Adding Footer component

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import HomePage from './pages/HomePage';
 import './styles/index.scss'
+import Footer from './components/Footer/Footer';
+import HomePage from './pages/HomePage';
 
+const currentYear: number = new Date().getFullYear();
 ReactDOM.render(
     <React.StrictMode>
 		<BrowserRouter>
             <Switch>
 				<Route path='/' exact component={HomePage} />
             </Switch>
+            <Footer copyrightInfo={`Copyright © ${currentYear} All Rights Reserved | Dev & Design by Bruno Veiga`} />
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('app-root')
-)
+);
