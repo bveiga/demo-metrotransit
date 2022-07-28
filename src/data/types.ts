231081c06d240a521202f560e914776b7c3999e0
Bruno Veiga <bspveiga@gmail.com>
2022-07-27 22:04:18 -0500

    Creating type interfaces for directions and stops

--- src/data/types.ts
+++ src/data/types.ts
@@ -1,5 +1,15 @@
 export interface TransitRoute {
-    route_id: string;
-    agency_id: number;
-    route_label: string;
+	route_id: string;
+	agency_id: number;
+	route_label: string;
+}
+
+export interface TransitDirection {
+	direction_id: number;
+	direction_name: string;
+}
+
+export interface TransitStop {
+	place_code: string;
+	description: string;
 }
