e654ed3f53e767a8e46746279efa2576d7aa5637
Bruno Veiga <bspveiga@gmail.com>
2022-07-28 09:48:44 -0500

    Adding more tests and the react testing library

--- src/components/RouteNavigator/RouteNavigator.spec.tsx
+++ src/components/RouteNavigator/RouteNavigator.spec.tsx
@@ -1,18 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import {act} from 'react-dom/test-utils';
+import {fireEvent} from '@testing-library/react';
 import { TransitDirection, TransitRoute, TransitStop } from '../../data/types';
 import RouteNavigator from './RouteNavigator';
 
 describe('Components | RouteNavigator', () => {
-    it('renders without crashing', () => {
-        const container = document.createElement('div');
-        document.body.appendChild(container);
+	let container: HTMLDivElement;
+	let component: Element;
+	
+	beforeAll(() => {
+		container = document.createElement('div');
+		document.body.appendChild(container);
+		
+		act(() => {
+			ReactDOM.render(<RouteNavigator />, container);
+			component = container.getElementsByClassName('route-navigator')[0];
+		});
+	});
 
-        act(() => {
-            ReactDOM.render(<RouteNavigator/>, container);
-        })
+	it('renders without crashing', () => {
+		expect(component).not.toBeNull();
+	});
 
-        expect(container).not.toBeNull();
-    });
+	it('renders the route selector', () => {
+		let routeSelectorSection = component.getElementsByClassName('route-selector')[0];
+		expect(routeSelectorSection).not.toBeNull();
+	});
+
+	it('renders the route display', () => {
+		let routeDisplaySection = component.getElementsByClassName('route-display')[0];
+		expect(routeDisplaySection).not.toBeNull();
+	});
+
+	it('loads routes', () => {
+		let routeSelectorSection = component.getElementsByClassName('route-selector')[0];
+		let routeSelect = routeSelectorSection.getElementsByTagName('select')[0];
+		let options = routeSelect.getElementsByTagName('option');
+
+		expect(options.length).toBeGreaterThan(0);
+	});
+
+	it('can select a new route', () => {
+		const routeSelectorSection = component.getElementsByClassName('route-selector')[0];
+		const routeSelect = routeSelectorSection.getElementsByTagName('select')[0];
+		let options = routeSelect.getElementsByTagName('option');
+		let middleOptionLabel = options[options.length / 2].value;
+		
+		expect(routeSelect.value).not.toBe(middleOptionLabel);
+		fireEvent.change(routeSelect), {target: {value: middleOptionLabel}};
+		expect(routeSelect.value).toBe(middleOptionLabel);
+	});
 });
--- src/components/RouteNavigator/RouteNavigator.tsx
+++ src/components/RouteNavigator/RouteNavigator.tsx
@@ -2,37 +2,39 @@ import React, { FC, useEffect, useState } from 'react';
 import { TransitDirection, TransitRoute, TransitStop } from '../../data/types';
 import './RouteNavigator.scss';
 
-import { routeData, directionData, stopData } from '../../data/sampleData';
+import { routeData, directionData, stopData } from '../../data/mockData';
 
 interface HeaderProps {
-    initialActiveStop?: number;
+	initialActiveStop?: number;
 	initialActiveTab?: number;
 }
 
 const RouteNavigator: FC<HeaderProps> = ({
-    initialActiveStop,
-    initialActiveTab = 0
+	initialActiveStop,
+	initialActiveTab = 0
 }) => {
-    const [state, setState] = useState({
-        activeRoute: routeData[0].route_label,
-        activeStop: initialActiveStop,
-        activeTab: initialActiveTab,
-        directions: directionData,
+	const [state, setState] = useState({
+		activeRoute: routeData[0].route_label,
+		activeStop: initialActiveStop,
+		activeTab: initialActiveTab,
+		directions: directionData,
 		routes: routeData,
-        stops: stopData
+		stops: stopData
 	});
 
-    const selectRoute = (evt: React.ChangeEvent<HTMLSelectElement>): void => {
-        let label = evt.target.value;
-        setState({ ...state, activeRoute: label});
-    }
+	const selectRoute = (evt: React.ChangeEvent<HTMLSelectElement>): void => {
+		console.log('Change to: '+evt.target.value);
 
-    const selectDirection = (index: number): void => {
-        setState({ ...state, activeTab: index });
-    };
+		let label = evt.target.value;
+		setState({ ...state, activeRoute: label});
+	}
 
-    const renderDirections = (direction: TransitDirection, index: number, activeIndex?: number): JSX.Element => {
-		const compClasses = (index === activeIndex) ? 'is-active' : '';
+	const selectDirection = (index: number): void => {
+		setState({ ...state, activeTab: index });
+	};
+
+	const renderDirections = (direction: TransitDirection, index: number, activeIndex?: number): JSX.Element => {
+		const compClasses = (index === activeIndex) ? 'direction is-active' : 'direction';
 		return (
 			<li className={compClasses} key={index} data-id={direction.direction_id} onClick={() => selectDirection(index)}>
 				<a>{direction.direction_name}</a>
@@ -40,32 +42,32 @@ const RouteNavigator: FC<HeaderProps> = ({
 		);
 	};
 
-    return (
-        <>
-            <section className='section route-selector'>
-                <h1 className='title has-text-light'>Route Selector</h1>
-                <h2 className='subtitle has-text-light'>
-                    Select a route
-                </h2>
-                <div className="select">
-                    <select onChange={selectRoute}>
-                        {state.routes.map((route) => {
-                            return (<option key={route.route_id}>{ route.route_label }</option>);
-                        })}
-                    </select>
-                </div>
-            </section>
-            <section className='section route-display'>
-                <h2 className='is-size-4'>
-                    {state.activeRoute}
-                </h2>
-                <div className='tabs is-toggle'>
-                    <ul>
-                        {state.directions.map((direction, index) => renderDirections(direction, index, state.activeTab))}
-                    </ul>
-                </div>
-            </section>
-        </>
+	return (
+		<div className="route-navigator">
+			<section className='section route-selector'>
+				<h1 className='title has-text-light'>Route Selector</h1>
+				<h2 className='subtitle has-text-light'>
+					Select a route
+				</h2>
+				<div className="select">
+					<select onChange={selectRoute}>
+						{state.routes.map((route) => {
+							return (<option key={route.route_id}>{ route.route_label }</option>);
+						})}
+					</select>
+				</div>
+			</section>
+			<section className='section route-display'>
+				<h2 className='is-size-4'>
+					{state.activeRoute}
+				</h2>
+				<div className='tabs is-toggle'>
+					<ul>
+						{state.directions.map((direction, index) => renderDirections(direction, index, state.activeTab))}
+					</ul>
+				</div>
+			</section>
+		</div>
 	);
 }
 
