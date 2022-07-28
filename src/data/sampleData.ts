e47e9afc96ca2c516e6cebef07659b367661ef4e
Bruno Veiga <bspveiga@gmail.com>
2022-07-27 22:04:04 -0500

    Consolidating sample data

---
+++ src/data/sampleData.ts
@@ -0,0 +1,58 @@
+export const routeData = [
+	{
+		"route_id":"901",
+		"agency_id":0,
+		"route_label":"METRO Blue Line"
+	},
+	{
+		"route_id":"902",
+		"agency_id":0,
+		"route_label":"METRO Green Line"
+	},
+	{
+		"route_id":"904",
+		"agency_id":0,
+		"route_label":"METRO Orange Line"
+	},
+	{
+		"route_id":"903",
+		"agency_id":0,
+		"route_label":"METRO Red Line"
+	},
+	{
+		"route_id":"921",
+		"agency_id":0,
+		"route_label":"METRO A Line"
+	},
+	{
+		"route_id":"923",
+		"agency_id":0,
+		"route_label":"METRO C Line"
+	},
+];
+
+export const directionData = [
+    {
+        "direction_id": 0,
+        "direction_name": "Northbound"
+    },
+    {
+        "direction_id": 1,
+        "direction_name": "Southbound"
+    }
+];
+
+export const stopData = [
+    {
+        "place_code": "HHTE",
+        "description": "MSP Airport Terminal 2 - Humphrey Station"
+    },
+    {
+        "place_code": "LIND",
+        "description": "MSP Airport Terminal 1 - Lindbergh Station"
+    },
+    {
+        "place_code": "FTSN",
+        "description": "Fort Snelling Station"
+    }
+];
