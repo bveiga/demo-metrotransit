9607c286a8f498fd69945c2487447a789d4311da
Bruno Veiga <bspveiga@gmail.com>
2022-07-28 16:10:31 -0500

    Adding mock data for departure times

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -56,3 +56,56 @@ export const stopData = [
         "description": "Fort Snelling Station"
     }
 ];
+
+export const departureData = {
+    "stops": [
+        {
+            "stop_id": 51435,
+            "latitude": 44.874119,
+            "longitude": -93.224068,
+            "description": "Terminal 2 Humphrey Station"
+        }
+    ],
+    "alerts": [],
+    "departures": [
+        {
+            "actual": true,
+            "trip_id": "21226119-JUN22-RAIL-Weekday-02",
+            "stop_id": 51435,
+            "departure_text": "7 Min",
+            "departure_time": 1659042240,
+            "description": "to Mpls-Target Field",
+            "route_id": "901",
+            "route_short_name": "Blue",
+            "direction_id": 0,
+            "direction_text": "NB",
+            "schedule_relationship": "Scheduled"
+        },
+        {
+            "actual": false,
+            "trip_id": "21226123-JUN22-RAIL-Weekday-02",
+            "stop_id": 51435,
+            "departure_text": "4:16",
+            "departure_time": 1659042960,
+            "description": "to Mpls-Target Field",
+            "route_id": "901",
+            "route_short_name": "Blue",
+            "direction_id": 0,
+            "direction_text": "NB",
+            "schedule_relationship": "Scheduled"
+        },
+        {
+            "actual": false,
+            "trip_id": "21226124-JUN22-RAIL-Weekday-02",
+            "stop_id": 51435,
+            "departure_text": "4:28",
+            "departure_time": 1659043680,
+            "description": "to Mpls-Target Field",
+            "route_id": "901",
+            "route_short_name": "Blue",
+            "direction_id": 0,
+            "direction_text": "NB",
+            "schedule_relationship": "Scheduled"
+        }
+	]
+};
--- src/data/types.ts
+++ src/data/types.ts
@@ -2,14 +2,28 @@ export interface TransitRoute {
 	route_id: string;
 	agency_id: number;
 	route_label: string;
-}
+};
 
 export interface TransitDirection {
 	direction_id: number;
 	direction_name: string;
-}
+};
 
 export interface TransitStop {
 	place_code: string;
 	description: string;
-}
+};
+
+export interface TransitDeparture {
+	actual: Boolean;
+	trip_id: string;
+	stop_id: number;
+	departure_text: string;
+	departure_time: number;
+	description: string;
+	route_id: string;
+	route_short_name: string;
+	direction_id: number;
+	direction_text: string;
+	schedule_relationship: string;
+};
