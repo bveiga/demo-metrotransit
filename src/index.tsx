36d23b9ff99383043d4cc23333b7fb461c3e8a87
Bruno Veiga <bspveiga@gmail.com>
2022-07-31 19:48:12 -0500

    Removing homepage component for simplicity

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './styles/index.scss'
 import Footer from './components/Footer/Footer';
-import HomePage from './pages/HomePage';
+import RouteNavigator from './components/RouteNavigator/RouteNavigator';
 
 const currentYear: number = new Date().getFullYear();
 ReactDOM.render(
     <React.StrictMode>
 		<BrowserRouter>
             <Switch>
-				<Route path='/' exact component={HomePage} />
+				<Route path='/' exact component={RouteNavigator} />
             </Switch>
             <Footer copyrightInfo={`Copyright © ${currentYear} All Rights Reserved | Dev & Design by Bruno Veiga`} />
         </BrowserRouter>
